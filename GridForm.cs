970504c6e7966fcaa2521b7aca6f2dd3e2ddc327
Alexey Dulayev <dulayev@gmail.com>
2019-02-22 00:03:36 +0300

    Extract GridForm from QueryForm
    Stuff specific to layout GridView and some controls above
    is extracted to base class GridForm.

--- GridForm.cs
+++ GridForm.cs
@@ -1,31 +1,30 @@
-﻿using System;
-using System.Data;
-using System.Data.OleDb;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Home_Accounting
 {
-    internal class QueryForm : Form
+    internal class GridForm : Form
     {
-        private Control[] controls;
-        private OleDbCommand cmd;
-        private DataGridView dataGridView1;
-        private FlowLayoutPanel flowLayoutPanel1;
+        private readonly Control[] controls;
+        private readonly DataGridView dataGridView1;
+        private readonly FlowLayoutPanel flowLayoutPanel1;
         private System.Drawing.Size defaultSize = new System.Drawing.Size(438, 127);
 
-        DataTable dataTable = new DataTable();
+        public DataGridView GridView { get { return dataGridView1; } }
 
-        public QueryForm(Control[] controls, OleDbCommand cmd)
+        public GridForm(Control[] controls)
         {
             this.controls = controls;
-
-            this.cmd = cmd;
 
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
 
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            InitLayout(controls);
+        }
+
+        private void InitLayout(Control[] controls)
+        {
             this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
 
             int controlsHeight = 0;
@@ -63,33 +62,15 @@ namespace Home_Accounting
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
 
             this.ClientSize = defaultSize;
-            this.ControlBox = false;
+            this.ShowInTaskbar = false;
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Text = "Caption";
-            this.Load += new System.EventHandler(this.OnLoad);
 
+            this.flowLayoutPanel1.ResumeLayout(true);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-        }
-
-        private void OnLoad(object sender, EventArgs e)
-        {
-            Reload();
-        }
-
-        public OleDbCommand DbCommand { get { return cmd; } }
-
-        public void Reload()
-        {
-            dataTable.Clear();
-            if (!string.IsNullOrEmpty(cmd.CommandText))
-            {
-                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
-                adapter.Fill(dataTable);
-                dataGridView1.DataSource = dataTable;
-            }
         }
     }
 }
---
+++ QueryForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace Home_Accounting
+{
+    internal class QueryForm : GridForm
+    {
+        private OleDbCommand cmd;
+
+        DataTable dataTable = new DataTable();
+
+        public QueryForm(Control[] controls, OleDbCommand cmd) : base(controls)
+        {
+            this.cmd = cmd;
+            this.Load += new System.EventHandler(this.OnLoad);
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            Reload();
+        }
+
+        public OleDbCommand DbCommand { get { return cmd; } }
+
+        public void Reload()
+        {
+            dataTable.Clear();
+            if (!string.IsNullOrEmpty(cmd.CommandText))
+            {
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                adapter.Fill(dataTable);
+                base.GridView.DataSource = dataTable;
+            }
+        }
+    }
+}
